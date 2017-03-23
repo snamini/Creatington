4b3fb5d6fe92a04502367b1d645c780e778332a5
Talia Namini <talia.namini@gmail.com>
2017-03-22 17:38:58 -0700

    fixed portfolio and search pages

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -5,25 +5,28 @@ import {SideNav, SideNavItem, Button} from 'react-materialize';
 export default class MySideNav extends React.Component {
   render(){
     return (
-      <SideNav
-        trigger={<Button>SIDE NAV DEMO</Button>}
-        options={{ closeOnClick: true }}
-        className = "fixed"
-        >
-        <SideNavItem userView
-          user={{
-            background: 'assets/office.jpg',
-            image: 'assets/yuna.jpg',
-            name: 'John Doe',
-            email: 'jdandturk@gmail.com'
+        <SideNav
+          // trigger={<Button>SIDE NAV DEMO</Button>}
+          options={{
+            menuWidth: 300,
+            closeOnClick: true
           }}
-        />
-        <SideNavItem href='#!icon' icon='cloud'>First Link With Icon</SideNavItem>
-        <SideNavItem href='#!second'>Second Link</SideNavItem>
-        <SideNavItem divider />
-        <SideNavItem subheader>Subheader</SideNavItem>
-        <SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem>
-      </SideNav>
+          className = "side-nav"
+          >
+          <SideNavItem userView
+            user={{
+              background: 'assets/office.jpg',
+              image: 'assets/yuna.jpg',
+              name: 'John Doe',
+              email: 'jdandturk@gmail.com'
+            }}
+          />
+          <SideNavItem href='#!icon' icon='cloud'>First Link With Icon</SideNavItem>
+          <SideNavItem href='#!second'>Second Link</SideNavItem>
+          <SideNavItem divider />
+          <SideNavItem subheader>Subheader</SideNavItem>
+          <SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem>
+        </SideNav>
     );
   }
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -5,6 +5,7 @@ import { Route, IndexRoute } from 'react-router';
 import Home from './containers/Home';
 import Portfolio from './containers/Portfolio';
 import Messages from './containers/Messages';
+import Search from './containers/Search';
 import Register from './containers/login/Register';
 import Login from './containers/login/Login';
 //Auth0
@@ -18,6 +19,7 @@ export default () => {
   		<Route path="/portfolio" component={Portfolio} />,
   		<Route path="/messages" component={Messages} />,
   		<Route path="/register" component={Register} />,
-  		<Route path="/login" component={Login} />
+  		<Route path="/login" component={Login} />,
+      <Route path="/search" component={Search} />
   ];
 }
