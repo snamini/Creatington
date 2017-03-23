a631e393754180707f6d884938c8ba7a21c6789b
Elena Volskaia <elenavolskaia@Elenas-MacBook-Pro.local>
2017-03-22 18:08:53 -0700

    Login files

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
--- src/containers/Container.js
+++ src/containers/Container.js
@@ -1,6 +1,7 @@
-import React, { PropTypes as T } from 'react'
-import { Jumbotron } from 'react-bootstrap'
-import styles from '../styles/styles.module.css'
+ import React from 'react'
+import { Jumbotron } from 'react-bootstrap';
+import Header from '../components/Header';
+//import styles from '../styles/styles.module.css'
 
 export class Container extends React.Component {
   render() {
@@ -13,7 +14,7 @@ export class Container extends React.Component {
 
     return (
       <Jumbotron>
-
+        <Header isLoggedIn={this.props.route.auth.loggedIn()}/>
         {children}
       </Jumbotron>
     )
